609317daf03d1e12aace87745551c164834a10cc
Saugat Rai <saugat.rai@intimetec.com>
2024-07-26 13:11:33 +0530

    should throw error if input is null or undefined

--- src/simple-calculator.test.ts
+++ src/simple-calculator.test.ts
@@ -1,7 +1,21 @@
 import { SimpleCalculator } from "./simple-calculator";
 
-describe("SimpleCalculator", () => {
+describe("AddFunctionErrorValidation", () => {
     const simpleCalculator = new SimpleCalculator();
+
+    it("Add function should not take undefined values", () => {
+        expect(() => {
+            const testObj:any = {};
+            simpleCalculator.add(testObj.undefined);
+          }).toThrow("Input string cannot be null|undefined");
+    })
+
+    it("Add function should not take null values", () => {
+        expect(() => {
+            const testObj:any = {value: null};
+            simpleCalculator.add(testObj.value);
+          }).toThrow("Input string cannot be null|undefined");
+    })
 
     it("Add function should take non empty string", () => {
         expect(() => {
--- src/simple-calculator.ts
+++ src/simple-calculator.ts
@@ -1,5 +1,8 @@
 export class SimpleCalculator {
     public add(numbers: string): number {
+        if(typeof(numbers) === 'undefined' || numbers === null){
+            throw new Error("Input string cannot be null|undefined");
+        }
         if(numbers.trim() == ""){
             throw new Error("Input string cannot be empty");
         }
